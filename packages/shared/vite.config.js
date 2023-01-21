98f5de480a6b274c0e9183ef99149817c4de6ba1
Susanna Kosic <susanna.kosic@gmail.com>
2023-01-21 18:58:59 +0000

    poc: shared package - setting remote cors

--- packages/shared/vite.config.js
+++ packages/shared/vite.config.js
@@ -2,33 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 import dns from 'dns'
+import * as dotenv from 'dotenv';
 
 dns.setDefaultResultOrder('verbatim')
-
+const config = dotenv.config();
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'shared',
-      filename: 'shared.js',
-      exposes: {
-        './Button': './src/components/Button'
-      },
-      shared: ["react"]
-    })
-  ],
-  preview: {
-    host: 'localhost',
-    port: 5000,
-    strictPort: true,
-    cors: {
-      origin: 'http://localhost:5001',
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  console.log(`command: ${command}, mode: ${mode}, ssrBuild: ${ssrBuild}`);
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'shared',
+        filename: 'shared.js',
+        exposes: {
+          './Button': './src/components/Button'
+        },
+        shared: ["react"]
+      })
+    ],
+    preview: {
+      host: 'localhost',
+      port: 5000,
+      strictPort: true,
+      cors: {
+        origin: config.APP_URL,
+      }
+    },
+    build: {
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false
     }
-  },
-  build: {
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false
   }
 })
