cf6f4392e05fa079c410e263c79497a08eefd5b0
Susanna Kosic <susanna.kosic@gmail.com>
2023-01-22 10:17:23 +0000

    poc: update shared package

--- packages/shared/src/components/Button.jsx
+++ packages/shared/src/components/Button.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import "./Button.css"
 
-export default ({caption = "Shared Button"}) => <button className='shared-btn'>{caption}</button>;
+export default ({ caption = "Shared Button", onClick }) => <button className='shared-btn' onClick={ onClick ? onClick : () => true}>{caption}</button>;
